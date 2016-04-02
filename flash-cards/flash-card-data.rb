8b21cfb2072d9447dcf434a105d4ed30f4498f52
einarstensson <einar.stensson@gmail.com>
2016-04-02 14:16:27 -0400

    Add output entire database and all commands by data type functions

--- flash-cards/flash-card-data.rb
+++ flash-cards/flash-card-data.rb
@@ -1,15 +1,41 @@
+=begin
+This is a set of functions that will retrieve data from my flash card database. 
+The idea is to expand this to a program that can be run from the terminal. I'd like 
+to expand this later, probably using meta-programming.
+=end
 
 require 'json'
 
+#Fetch flash card data
 file = File.read('flash-card-data.json')
-
 flash_card_data = JSON.parse(file)
 
-flash_card_data.each do |type, values|
-  values.each_with_index do |command, index|
-    puts "\n------------ COMMAND: #{type.to_s} ##{index + 1}------------"
-    puts "Variables: #{command['variables']}" unless command['variables'].empty?
-    puts command['command']
-    puts "===>\n #{command['output']}"
+#-------------- OUTPUT ENTIRE DATABASE --------------
+def all(flash_card_data)
+  flash_card_data.each do |type, values|
+
+    values.each_with_index do |command, index|
+      puts "\n------------ COMMAND: #{type.to_s} ##{index + 1}------------"
+      puts "Variables: #{command['variables']}" unless command['variables'].empty?
+      puts command['command']
+      puts "===>\n #{command['output']}"
+    end
+
   end
 end
+
+#-------------- OUTPUT ALL COMMANDS OF A CERTAIN DATA TYPE --------------
+def type(flash_card_data, data_type)
+  puts "\n------------ COMMAND: #{data_type} ------------\n"
+
+  flash_card_data[data_type].each do |command|
+      puts "Variables: #{command['variables']}" unless command['variables'].empty?
+      puts command['command']
+      puts "===>\n #{command['output']}\n\n"    
+  end
+
+end
+
+#TEST RUNS
+#all(flash_card_data)
+type(flash_card_data, "string")
