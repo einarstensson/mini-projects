6444b64b1ae807cd170ff84021de34cc8bf322bd
einarstensson <einar.stensson@gmail.com>
2016-04-03 12:27:25 -0400

    Add a command specific search and create a print command helper method.

--- flash-cards/flash-card-data.rb
+++ flash-cards/flash-card-data.rb
@@ -4,6 +4,7 @@ The idea is to expand this to a program that can be run from the terminal. I'd l
 to expand this later, probably using meta-programming.
 =end
 
+require 'pry'
 require 'json'
 
 #Fetch flash card data
@@ -29,13 +30,37 @@ def type(flash_card_data, data_type)
   puts "\n------------ COMMAND: #{data_type} ------------\n"
 
   flash_card_data[data_type].each do |command|
-      puts "Variables: #{command['variables']}" unless command['variables'].empty?
-      puts command['command']
-      puts "===>\n #{command['output']}\n\n"    
+    print_command(command)
   end
 
 end
 
+#-------------- OUTPUT SPECIFIC COMMAND --------------
+def command(flash_card_data, command_name)
+  search_result = []
+
+  flash_card_data.each do |data_type, commands|
+    result = commands.select{ |command| command['name'] == command_name }
+    search_result << result unless result == []
+  end
+
+  search_result.flatten.each do |command|
+    print_command(command)
+  end
+end
+
+#-------------- HELPER METHODS --------------
+def print_command(command)
+  puts
+  puts "Variables: #{command['variables']}" unless command['variables'].empty?
+  puts command['command']
+  puts "===>\n #{command['output']}\n\n"
+end
+
 #TEST RUNS
 #all(flash_card_data)
-type(flash_card_data, "string")
+#type(flash_card_data, "string")
+#command(flash_card_data, '?')
+
+
+
